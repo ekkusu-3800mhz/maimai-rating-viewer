bbac0a3fa5c391cae708f969f423a3ba7e88603f
Shinra DX <shinra.dx@outlook.com>
2021-11-20 01:48:43 +0800

    封装查分器接口请求

---
+++ web/src/app/data/maidx-prober/index.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosPromise } from 'axios'
+import config from '@/app.config'
+
+/**
+ *  舞萌DX底分对象查看器 - 查分器数据接口封装类
+ * 
+ *  @author X-buster <shinra.dx@outlook.com>
+ *  @license MIT
+ */
+
+export default class MaiDXProberApi {
+
+    /**
+     *  用户名存储变量
+     * 
+     *  @var {string}
+     */
+
+    private userName: string = ''
+
+    /**
+     *  构造函数
+     * 
+     *  @param {string} userName 用户名
+     */
+
+    public constructor(userName: string) {
+        this.userName = userName
+    }
+
+    /**
+     *  获取B40数据
+     * 
+     *  @returns {AxiosPromise<unknown>}
+     */
+
+    public b40(): AxiosPromise<unknown> {
+        return this.request({
+            userName: this.userName
+        })
+    }
+
+    /**
+     *  获取B50数据
+     * 
+     *  @returns {AxiosPromise<unknown>}
+     */
+
+    public b50(): AxiosPromise<unknown> {
+        return this.request({
+            userName: this.userName,
+            b50: "TRUE"
+        })
+    }
+
+    /**
+     *  通用接口请求封装
+     * 
+     *  @param {Record<string, string>} data 请求参数
+     *  @returns {AxiosPromise<unknown>}
+     */
+
+    private request(data: Record<string, string>): AxiosPromise<unknown> {
+        return axios.post(config.data.prober, {data})
+    }
+
+}
---
+++ web/src/app/data/maidx-prober/interface.ts
@@ -0,0 +1,49 @@
+/**
+ *  舞萌DX底分对象查看器 - 查分器接口数据结构定义
+ * 
+ *  @author X-buster <shinra.dx@outlook.com>
+ *  @license MIT
+ */
+
+// 一般错误的返回结构
+export interface IBadRequest {
+    message: string
+}
+
+// 隐私设置错误的返回结构
+export interface IForbidden {
+    status: string
+    msg: string
+}
+
+// 请求成功的返回结构
+export interface IResponse {
+    user_id: unknown | null
+    user_data: unknown | null
+    username: string
+    nickname: string
+    rating: number
+    plate: string
+    additional_rating: number
+    charts: {
+        dx: Array<IMusicChart>
+        sd: Array<IMusicChart>
+    }
+}
+
+// 谱面数据结构
+export interface IMusicChart {
+    achievements: number
+    ds: number
+    dxScore: number
+    fc: string
+    fs: string
+    level: string
+    level_index: number
+    level_label: string
+    ra: number
+    rate: string
+    song_id: number
+    title: string
+    type: string
+}
