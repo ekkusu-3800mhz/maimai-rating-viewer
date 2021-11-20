332f376a7ba092685fab89f5e8bfc23f91ff2c86
Shinra DX <shinra.dx@outlook.com>
2021-11-20 13:51:25 +0800

    使用Google Fonts的在线字体

--- web/src/app/App.vue
+++ web/src/app/App.vue
@@ -8,7 +8,6 @@
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { decode } from "js-base64";
 import Cookie from "js-cookie";
 import MaiDXProberApi from "@/app/data/maidx-prober";
 
@@ -16,7 +15,7 @@ const { push, currentRoute } = useRouter();
 const store = useStore();
 
 if (currentRoute.value.name != "ResultShare") {
-    let userName: string = decode(Cookie.get("USER_NAME") as string);
+    let userName: string = Cookie.get("USER_NAME") as string
     const prober = new MaiDXProberApi(userName);
     (prober.b40()).then(() => {
         store.commit("setUserName", userName);
@@ -34,6 +33,6 @@ if (currentRoute.value.name != "ResultShare") {
 
 <style lang="less">
 body {
-    font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Noto Sans SC', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif !important;
+    font-family: Roboto, Noto Sans SC, Noto Sans JP, Microsoft YaHei, '微软雅黑', Arial, sans-serif !important;
 }
 </style>
