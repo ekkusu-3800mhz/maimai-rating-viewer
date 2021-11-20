eb581a910091dc29dd2e5f1d74323d795bcd00a5
Shinra DX <shinra.dx@outlook.com>
2021-11-20 13:51:47 +0800

    引入Element Plus

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,9 @@ import App from "@/app/App.vue";
 import router from "@/app/core/router";
 import store from "@/app/core/store";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 /**
  *  舞萌DX底分对象查看器 - 应用入口文件
  * 
@@ -10,4 +13,4 @@ import store from "@/app/core/store";
  *  @license MIT
  */
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
