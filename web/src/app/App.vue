ae91c2f7c601889b015d69fa49579217c0621c73
Shinra DX <shinra.dx@outlook.com>
2021-11-20 17:15:40 +0800

    设置全局文字粗细

--- web/src/app/App.vue
+++ web/src/app/App.vue
@@ -33,6 +33,8 @@ if (currentRoute.value.name != "ResultShare") {
 
 <style lang="less">
 body {
+    background-color: #fcfcfc;
     font-family: Roboto, Noto Sans SC, Noto Sans JP, Microsoft YaHei, '微软雅黑', Arial, sans-serif !important;
+    font-weight: 300;
 }
 </style>
