d67bad94bd1f4fc03cfa54c03003031cb3a7bd75
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:44:45 +0800

    定义页脚组件

---
+++ web/src/app/components/frameworks/Footer.vue
@@ -0,0 +1,31 @@
+<!--
+    舞萌DX底分对象查看器 - 页脚组件视图
+
+    @author X-buster <shinra.dx@outlook.com>
+    @license MIT
+-->
+
+<template>
+    <div class="page-footer">
+        <p>Made by <a href="https://github.com/sh1nra-dx" target="_blank">Shinra DX</a> armed with X-buster.</p>
+        <p>Powered by <a href="https://v3.vuejs.org" target="_blank">Vue 3.x</a>, <a href="https://element-plus.gitee.io" target="_blank">Element<sup>+</sup></a>, and <a href="https://github.com/Diving-Fish/maimaidx-prober" target="_blank">Diving-Fish/maimaidx-prober</a>.</p>
+        <br>
+        <p>Copyrights of All resources from "maimaiでらっくす" is <a href="https://maimai.sega.jp" target="_blank">&copy;SEGA</a>.</p>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.page-footer {
+    text-align: center;
+    padding-top: 2rem;
+}
+p {
+    margin-bottom: -14px;
+    font-size: 16px;
+}
+@media (min-width: 768px) {
+    .page-footer {
+        padding-top: 3rem;
+    }
+}
+</style>
