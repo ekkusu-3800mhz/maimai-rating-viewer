677dced0ba6ee67c47d824e12e3a7f86c411d0b5
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:44:55 +0800

    定义用户名输入（登录）页头组件

---
+++ web/src/app/components/frameworks/LoginHeader.vue
@@ -0,0 +1,40 @@
+<!--
+    舞萌DX底分对象查看器 - 用户登录页头组件视图
+
+    @author X-buster <shinra.dx@outlook.com>
+    @license MIT
+-->
+
+<template>
+    <div class="page-header">
+        <p>
+            <img src="/images/logo.png" class="logo" alt="舞萌DX LOGO" />
+        </p>
+        <h1>MaiRating Viewer</h1>
+        <p></p>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.page-header {
+    text-align: center;
+}
+.page-header > h1 {
+    font-size: 28px;
+    font-weight: 300;
+}
+.logo {
+    padding-top: 2rem;
+    width: 10rem;
+}
+@media (min-width: 768px) {
+    .page-header > h1 {
+        font-size: 36px;
+        font-weight: 300;
+    }
+    .logo {
+        padding-top: 2rem;
+        width: 15rem;
+    }
+}
+</style>
