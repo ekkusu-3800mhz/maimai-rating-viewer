f45ce0d895f17c2eff4232a9c2727d3e9723ff2b
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:47:08 +0800

    定义DX Rating框组件

---
+++ web/src/app/components/elements/Rating.vue
@@ -0,0 +1,34 @@
+<!--
+    舞萌DX底分对象查看器 - DX Rating框组件
+
+    @author X-buster <shinra.dx@outlook.com>
+    @license MIT
+-->
+
+<script lang="ts" setup>
+import { ref, defineProps } from "vue";
+import { b40RatingColor, b50RatingColor } from "@/app/utils/asset-map";
+
+const props = defineProps<{
+    mode: string;
+    value: number;
+}>();
+
+const color = ref("");
+color.value = (props.mode === "b50") ? b50RatingColor(props.value) : b40RatingColor(props.value);
+</script>
+
+<template>
+    <img :src="color" alt="DX Rating" />
+</template>
+
+<style lang="less" scoped>
+img {
+    width: 170px;
+}
+@media (min-width: 768px) {
+    img {
+        width: 296px;
+    }
+}
+</style>
