9d9fa8613cb5d233b40618694e17bec42df70f07
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:43:58 +0800

    完善应用路由表

--- web/src/app/core/router/routes.ts
+++ web/src/app/core/router/routes.ts
@@ -1,5 +1,9 @@
 import { RouteRecordRaw } from "vue-router";
 
+import UserInput from "@/app/views/UserInput.vue";
+import RatingResult from "@/app/views/RatingResult.vue";
+import NotFound from "@/app/views/NotFound.vue";
+
 /**
  *  舞萌DX底分对象查看器 - 应用路由表定义文件
  * 
@@ -7,6 +11,19 @@ import { RouteRecordRaw } from "vue-router";
  *  @license MIT
  */
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [{
+    path: "/",
+    name: "UserInput",
+    component: UserInput,
+}, {
+    path: "/result/:mode",
+    name: "RatingResult",
+    component: RatingResult,
+    props: true,
+},{
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
+}];
 
 export default routes;
