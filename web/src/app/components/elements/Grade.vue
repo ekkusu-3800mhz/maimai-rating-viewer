b466d6976c9b3659debe7107cd1a881911f5b34a
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:46:51 +0800

    定义对战段位徽章组件

---
+++ web/src/app/components/elements/Grade.vue
@@ -0,0 +1,30 @@
+<!--
+    舞萌DX底分对象查看器 - 对战段位徽章组件
+
+    @author X-buster <shinra.dx@outlook.com>
+    @license MIT
+-->
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+import { grade } from "@/app/utils/asset-map";
+
+const props = defineProps<{
+    value: number;
+}>();
+</script>
+
+<template>
+    <img :src="grade(props.value)" alt="Grade" />
+</template>
+
+<style lang="less" scoped>
+img {
+    width: 110px;
+}
+@media (min-width: 768px) {
+    img {
+        width: 180px;
+    }
+}
+</style>
