06bb218f63844b2dfcbcdaa9684379fa8fe33f21
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:36:54 +0800

    定义404错误视图

---
+++ web/src/app/views/NotFound.vue
@@ -0,0 +1,41 @@
+<!--
+    舞萌DX底分对象查看器 - 404错误视图
+
+    @author X-buster <shinra.dx@outlook.com>
+    @license MIT
+-->
+
+<script lang="ts" setup>
+import LoginHeader from "@/app/components/frameworks/LoginHeader.vue";
+import Footer from "@/app/components/frameworks/Footer.vue";
+</script>
+
+<template>
+     <el-container>
+        <el-main>
+            <LoginHeader />
+            <div class="main-content">
+                <el-row justify="center">
+                    <el-col :xs="24" :sm="10">
+                        <el-alert title="您所访问的资源不存在。" type="error" effect="dark" :closable="false" show-icon />
+                    </el-col>
+                </el-row>
+            </div>
+            <Footer />
+        </el-main>
+    </el-container>
+</template>
+
+<style lang="less" scoped>
+.main-content {
+    padding-top: 2rem;
+    padding-bottom: 3rem;
+}
+
+@media (min-width: 768px) {
+    .main-content {
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+    }
+}
+</style>
