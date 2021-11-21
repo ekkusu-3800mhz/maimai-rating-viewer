c69ec35ab8b947131663f4adb518c50a33ed8973
Shinra DX <shinra.dx@outlook.com>
2021-11-21 23:35:25 +0800

    引入栅格布局

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,7 @@ import store from "@/app/core/store";
 
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/display.css";
 
 /**
  *  舞萌DX底分对象查看器 - 应用入口文件
