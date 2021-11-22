5e3d327f6d4bbf7561b816d3923e57ea68434b03
Shinra DX <shinra.dx@outlook.com>
2021-11-22 21:52:28 +0800

    实装分享功能

--- web/src/app/views/SharedResult.vue
+++ web/src/app/views/SharedResult.vue
@@ -1,5 +1,5 @@
 <!--
-    舞萌DX底分对象查看器 - DX Rating详情视图
+    舞萌DX底分对象查看器 - DX Rating分享页视图
 
     @author Shinra DX armed with X-buster <shinra.dx@outlook.com>
     @license MIT
@@ -8,99 +8,77 @@
 <script lang="ts" setup>
 import { ref, defineProps } from "vue";
 import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import { encode } from "js-base64";
-import { toClipboard } from "@soerenmartius/vue3-clipboard";
+import { decode } from "js-base64";
+import { ElLoading } from "element-plus";
 import { sumRating } from "@/app/utils/b50-rating";
+import { isDebugMode } from "@/app/utils/functions";
 import { IMusicChart, IResponse } from "@/app/data/maidx-prober/interface";
 import Api from "@/app/data/api";
+import { ISharedResultResponse } from "@/app/data/api/interface";
 import { AxiosError, AxiosResponse } from "axios";
-import { IResultCacheRequest, IResultCacheResponse } from "@/app/data/api/interface";
-import { buildShareUrl, isDebugMode } from "@/app/utils/functions";
 
 import Footer from "@/app/components/frameworks/Footer.vue";
+import PageTitle from "../components/frameworks/PageTitle.vue";
 import PlayerInfo from "@/app/components/frameworks/PlayerInfo.vue";
-import PageTitle from "@/app/components/frameworks/PageTitle.vue";
 import MusicChart from "@/app/components/frameworks/MusicChart.vue";
 
+
 const props = defineProps<{
-    mode: string;
+    resultId: string;
 }>();
 
 const { push } = useRouter();
-const store = useStore();
+
+const errDialog = ref(false);
+const errMsg = ref("");
+const loading = ref(true);
 
 const title = ref("");
 const nickName = ref("");
+const mode = ref("");
 const legacyCharts = ref<Array<IMusicChart>>([]);
 const currentCharts = ref<Array<IMusicChart>>([]);
 const b40BaseRating = ref(0);
 const b40GradeRating = ref(0);
 const b50Rating = ref(0);
 
-if ( (props.mode !== "b40" && props.mode !== "b50") || (store.state.proberData === {})
-    || (store.state.userName === '') ) {
+if (props.resultId === "") {
     push({name: "UserInput"});
 } else {
-    let proberData: IResponse = store.state.proberData;
-    nickName.value = proberData.nickname;
-    legacyCharts.value = proberData.charts.sd;
-    currentCharts.value = proberData.charts.dx;
-    if (props.mode === "b50") {
-        title.value = "Best 50模拟";
-        let b35: number = sumRating(legacyCharts.value);
-        let b15: number = sumRating(currentCharts.value);
-        b50Rating.value = b35 + b15;
-    } else {
-        title.value = "Best 40查询";
-        b40BaseRating.value = proberData.rating;
-        b40GradeRating.value = proberData.additional_rating;
-    }
-}
-
-function goBack(): void {
-    push({name: "UserInput"});
-}
-
-const shareCard = ref(false);
-const errMsg = ref("");
-const errAlert = ref(false);
-const loading = ref(false);
-const shareUrl = ref("");
-const copyBtnText = ref("复制");
-
-function share(): void {
-    loading.value = true;
-    let params: IResultCacheRequest = {
-        type: props.mode,
-        data: encode(JSON.stringify(store.state.proberData)),
-    };
-    let response = Api.save(params);
-    response.then((res: AxiosResponse<IResultCacheResponse>) => {
-        shareUrl.value = buildShareUrl(res.data.resultId);
-        shareCard.value = true;
+    let request = Api.load(props.resultId);
+    request.then((res: AxiosResponse<ISharedResultResponse>) => {
+        let data: unknown = JSON.parse(decode(res.data.data));
+        let proberData: IResponse = data as IResponse;
+        mode.value = res.data.type;
+        nickName.value = proberData.nickname;
+        legacyCharts.value = proberData.charts.sd;
+        currentCharts.value = proberData.charts.dx;
+        if (mode.value === "b50") {
+            title.value = "Best 50模拟";
+            let b35: number = sumRating(legacyCharts.value);
+            let b15: number = sumRating(currentCharts.value);
+            b50Rating.value = b35 + b15;
+        } else {
+            title.value = "Best 40查询";
+            b40BaseRating.value = proberData.rating;
+            b40GradeRating.value = proberData.additional_rating;
+        }
         loading.value = false;
     }).catch((err: AxiosError) => {
         if (typeof err.response === 'undefined') {
             errMsg.value = err.message;
         } else {
-            errMsg.value = err.response.data.error;
+            if (err.response.status === 404) {
+                push({name: "NotFound"});
+            } else {
+                errMsg.value = err.response.data.error;
+            }
         }
         if (!isDebugMode()) {
             errMsg.value = "服务端接口请求失败，请稍后再试";
         }
-        loading.value = false;
-        errAlert.value = true;
+        errDialog.value = true;
     });
-}
-
-function copyLink(): void {
-    const shareContent = `${nickName.value}的舞萌DX Rating对象列表 | ${shareUrl.value}`;
-    toClipboard(shareContent);
-    copyBtnText.value = "已复制";
-    setTimeout(() => {
-        copyBtnText.value = "复制";
-    }, 2000);
 }
 
 const legacyBtn = ref("primary");
@@ -122,29 +100,28 @@ function showCurrentChart(): void {
     currentChartStyle.value = "list-show";
 }
 
+function toUserInput() {
+    errDialog.value = false;
+    push({name: "UserInput"});
+}
+
+function goBack() {
+    push({name: "UserInput"});
+}
+
+function share() {
+    return false;
+}
+
 document.documentElement.scrollTop = document.body.scrollTop = 0;
 </script>
 
 <template>
     <el-container v-loading.fullscreen.lock="loading">
-        <el-main>
+        <el-main v-if="!loading">
             <div class="main-content">
-                <PageTitle :title="title" :back="goBack" :share="share" :share-show="!shareCard" />
-                <PlayerInfo :mode="props.mode" :nick-name="nickName" :b40-base-rating="b40BaseRating" :b40-grade-rating="b40GradeRating" :b50-rating="b50Rating" />
-                <el-row class="share-card" justify="center" v-if="shareCard">
-                    <el-col :xs="24" :sm="18" :md="16">
-                        <el-card>
-                            <el-result class="share-result" icon="success" title="保存成功" sub-title="您可通过复制以下链接进行分享" />
-                            <div style="margin-top: 15px">
-                                <el-input v-model="shareUrl" :readonly="true">
-                                    <template #append>
-                                        <el-button @click="copyLink()">{{copyBtnText}}</el-button>
-                                    </template>
-                                </el-input>
-                            </div>
-                        </el-card>
-                    </el-col>
-                </el-row>
+                <PageTitle :title="title" :back="goBack" :share="share" :share-show="false" />
+                <PlayerInfo :mode="mode" :nick-name="nickName" :b40-base-rating="b40BaseRating" :b40-grade-rating="b40GradeRating" :b50-rating="b50Rating" />
                 <el-row class="pagination" justify="center">
                     <el-col :xs="24" :sm="18" :md="16">
                         <el-card>
@@ -164,6 +141,14 @@ document.documentElement.scrollTop = document.body.scrollTop = 0;
         </el-main>
     </el-container>
     <el-backtop />
+    <el-dialog :fullscreen="true" v-model="errDialog" title="错误" :show-close="false" center>
+        <p style="text-align: center;">{{errMsg}}</p>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button type="danger" @click="toUserInput()">返回起始页</el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 
 <style lang="less" scoped>
@@ -171,19 +156,18 @@ document.documentElement.scrollTop = document.body.scrollTop = 0;
     padding-top: 2rem;
     padding-bottom: 3rem;
 }
-.pagination, .share-card {
+.pagination {
     padding-top: 1rem;
     text-align: center;
-}
-.share-result {
-    margin-top: -2rem;
-    margin-bottom: -2rem;
 }
 .list-show {
     display: block;
 }
 .list-hide {
     display: none;
+}
+.dialog-width {
+    width: 80%;
 }
 @media (min-width: 768px) {
     .main-content {
