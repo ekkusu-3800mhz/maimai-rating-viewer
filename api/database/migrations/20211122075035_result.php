46ff2b800f71e90c79034db671a9009303ed2bd8
Shinra DX <shinra.dx@outlook.com>
2021-11-22 16:49:12 +0800

    限制UUID字段长度

--- api/database/migrations/20211122075035_result.php
+++ api/database/migrations/20211122075035_result.php
@@ -11,11 +11,11 @@ use think\migration\db\Column;
  */
 
 class Result extends Migrator {
-    
+
     public function change() {
         ($this->table('result', ['id' => false, 'primary_key' => 'result_id']))
             ->addColumn('result_id', 'integer', ['null' => false, 'comment' => '成绩ID'])
-            ->addColumn('result_uuid', 'uuid', ['null' => false, 'comment' => '成绩UUID'])
+            ->addColumn('result_uuid', 'uuid', ['limit' => 36, 'null' => false, 'comment' => '成绩UUID'])
             ->addColumn('result_type', 'string', ['null' => false, 'comment' => '成绩类型'])
             ->addColumn('result_body', 'text', ['null' => false, 'comment' => '成绩原始数据包'])
             ->addColumn('result_save_date', 'date', ['null' => false, 'comment' => '成绩缓存时间戳'])
