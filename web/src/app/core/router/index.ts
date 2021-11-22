bab78551a7b3d3d81fc564a8cdf282fe9cf2e70d
Shinra DX <shinra.dx@outlook.com>
2021-11-22 21:49:47 +0800

    使用History路由模式

--- web/src/app/core/router/index.ts
+++ web/src/app/core/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 
 /**
@@ -9,6 +9,6 @@ import routes from "./routes";
  */
 
 export default createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 });
