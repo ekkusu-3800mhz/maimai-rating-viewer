4a7c67f2e6227408c56fc68703887defa5b85cc4
Shinra DX <shinra.dx@outlook.com>
2021-11-22 16:43:52 +0800

    封装接口响应函数

--- api/app/common.php
+++ api/app/common.php
@@ -1,2 +1,39 @@
 <?php
-// 应用公共文件
+declare(strict_types = 1);
+
+use think\Response;
+
+/**
+ *  舞萌DX底分对象查看器 - 公共函数定义
+ * 
+ *  @author Shinra DX armed with X-buster <shinra.dx@outlook.com>
+ *  @license MIT
+ */
+
+/**
+ *  构建成功响应数据包
+ * 
+ *  @param array $data 响应内容数组
+ *  @return think\Response
+ */
+
+function successResponse(array $data = array()): Response {
+    if (!empty($data)) {
+        $data = array(
+            'status' => 'OK',
+        );
+    }
+    return json($data, 200);
+}
+
+/**
+ *  构建失败响应数据包
+ * 
+ *  @param int $status 响应状态码
+ *  @param array $data 错误内容数组
+ *  @return think\Response
+ */
+
+function failedResponse(int $status = 500, array $error = array()): Response {
+    return json($error, $status);
+}
