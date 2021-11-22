b61e5561f9437e8c732dbd29574c456f72ba78a3
Shinra DX <shinra.dx@outlook.com>
2021-11-22 16:38:14 +0800

    定义Rating对象缓存数据模型类

---
+++ api/app/model/RatingCache.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\model;
+
+use think\Model;
+
+/**
+ *  舞萌DX底分对象查看器 - Rating对象缓存数据模型类
+ * 
+ *  @author Shinra DX armed with X-buster <shinra.dx@outlook.com>
+ *  @license MIT
+ */
+
+class RatingCache extends Model {
+    
+    // 指定数据表名称
+    protected $name = 'result';
+
+    // 指定主键名称
+    protected $pk = 'result_id';
+
+}
